23987f47d212b729a858732c238f7ce30ea1dba6
donghyeon-100 <goat.ohgiraffers@gmail.com>
2025-06-30 10:33:46 +0900

    폴더 삭제

--- scoula/frontend/src/pages/auth/JoinPage.vue
+++
@@ -1,5 +0,0 @@
-<script setup></script>
-
-<template>
-  <h1>JoinPage</h1>
-</template>
--- scoula/frontend/src/router/board.js
+++
@@ -1,9 +0,0 @@
-// 게시판 라우팅
-
-export default [
-  {
-    path: '/board/list',
-    name: 'board/list',
-    component: () => import('../pages/board/BoardListPage.vue'),
-  },
-];
